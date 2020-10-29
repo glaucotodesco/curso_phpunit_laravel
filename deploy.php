0bed3b6050c9413d7b5c58e7aeeb55cebcbe51bb
glauco.todesco <glauco.todesco@facens.br>
2020-10-28 22:15:30 -0300

    Fix deployer

--- deploy.php
+++ deploy.php
@@ -11,7 +11,7 @@ set('application', 'my_project');
 set('repository', 'https://github.com/glaucotodesco/curso_phpunit_laravel.git');
 
 // [Optional] Allocate tty for git clone. Default value is false.
-set('git_tty', true); 
+set('git_tty', false); 
 
 // Shared files/dirs between deploys 
 add('shared_files', []);
