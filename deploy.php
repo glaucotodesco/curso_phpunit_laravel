ab16cfefc4fa0d51f60044326e3460662d321a9a
glaucotodesco <glauco.todesco@gmail.com>
2020-11-01 20:09:29 -0300

    Update deploy.php

--- deploy.php
+++ deploy.php
@@ -12,7 +12,9 @@ set('repository', 'https://github.com/glaucotodesco/curso_phpunit_laravel.git');
 
 // [Optional] Allocate tty for git clone. Default value is false.
 set('git_tty', false); 
+
 set('writable_mode', 'chown'); 
+set('writable_chmod_mode', '0775');
 
 // Shared files/dirs between deploys 
 add('shared_files', []);
