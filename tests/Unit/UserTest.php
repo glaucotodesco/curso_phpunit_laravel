6406da3faaadcd47bba6bb4c6d50c6777d536e44
Glauco <glauco.todesco@abutua.com>
2020-12-07 19:01:49 -0300

    Caps 20 E 21

--- tests/Unit/UserTest.php
+++ tests/Unit/UserTest.php
@@ -5,15 +5,39 @@ namespace Tests\Unit;
 use PHPUnit\Framework\TestCase;
 use App\Models\User;
 
+use function PHPUnit\Framework\assertEquals;
+
+//https://www.youtube.com/playlist?list=PLfdtiltiRHWGXSggf05W-pJbD47-_d8bJ
+
+
 class UserTest extends TestCase
 {
+    protected $user;
+
+    public function setUp():void
+    {
+        $this->user = new User();
+        echo "\nTeste setUp";
+    }
     
     /** @test */
     public function checkIfUserColumnsIsCorrect()
     {
-        $user = new User();
+       
         $expected = ['name','email','password'];
-        $results = array_diff($expected, $user->getFillable());
+        $results = array_diff($expected, $this->user->getFillable());
         $this->assertEquals(0, count($results));
     }
+
+    /** @test */
+    public function checkIfFullNameIsCorrect(){
+        
+        $this->user->name = 'Glauco';
+        $this->user->lastName = 'Todesco';
+
+        assertEquals('Glauco Todesco',$this->user->getFullName());
+        
+    }
+
+
 }
